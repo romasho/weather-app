e223894a66ab2382f0052e07dfb3a6d812d0e13d
Roman <sholomitskyR@yandex.by>
2022-09-16 09:57:21 +0300

    Updates

--- weather-app/src/constants/index.ts
+++
@@ -1,9 +0,0 @@
-export enum AppRoutes {
-  ERROR = '*',
-  MAIN = '/',
-}
-
-export const tokenMapbox =
-  'pk.eyJ1Ijoicm9tYXNobyIsImEiOiJjbDM4b3J1eW0wMXlkM2tueHd3aDMzdnVmIn0.oTvcz02NvyWmUXSfbjYHYw';
-
-export const tokenOpenWeather = 'a4aea9a90180fe39dec1e62d7edd286b';
--- weather-app/src/pages/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as ErrorPage } from './404';
-export { default as MainPage } from './Main';
